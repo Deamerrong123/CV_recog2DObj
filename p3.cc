192d22dd9c15a7ccf3ddebba422801dcdb4dd70a
Rong97 <classof2017Deamerrong@gmail.com>
2023-09-25 11:38:16 -0400

    updated

--- p3.cc
+++ p3.cc
@@ -1,7 +1,6 @@
 // Qizhao Rong
 // p1.cc
 // 
-
 #include "image.h"
 #include <cstdio>
 #include <iostream>
@@ -25,14 +24,14 @@ int convert ( const int& pixel , const int& label){
 }
 
 
-void calculations(const Image& an_image,  const int& label , const int& n_rows, const int& n_cols){
+void calculations(const Image& an_image,  const int& label , const int& n_rows, const int& n_cols, std::vector<doubld> values){
   size_t i , j;
   int a_pr, b_pr, c_pr;
   double x_bar, y_bar , region_area , a, b ,c ;
   double theta_1, theta_2, e_min, e_max , roundedness, rho;
-  std::vector<double> values{7};
   a_pr = b_pr = c_pr = region_area;
   x_bar = y_bar = 0;
+  values[0] = label;
 
   for (i = 0 ; i < n_row ; ++i){
     for ( j = 0 ; j < n_cols ; ++j){
@@ -48,22 +47,35 @@ void calculations(const Image& an_image,  const int& label , const int& n_rows, 
 
   b_pr = b_pr * 2; // b_pr = 2 * \sum {\sum { i * j * b_ij}};
   x_bar = x_bar / region_area;
+  values[1] = x_bar;
+
   y_bar = y_bar / region_area;
+  values[2] = y_bar;
 
   a = a_pr - x_bar * x_bar * region_area;
   b = b_pr - 2 * x_bar * y_bar * region_area;
   c = c_pr - y_bar * y_bar * regions_area;
   theta_1 = atan2( b , a - c) / 2.0; // theta1 in radians
   e_min = a * sin( theta_1 ) * sin( theta_1 ) - b * sin( theta_1 ) * cos( theta_1 ) + c* cos (theta_1) * cos(theta_1);
+  values[3] = e_min;
+  values[4] = region_area;
   theta_2 = theta_1 + M_PI / 2.0;
   e_max = a * sin( theta_2 ) * sin( theta_2 ) - b * sin( theta_2 ) * cos( theta_2 ) + c* cos (theta_2) * cos(theta_2);
   roundedness = e_min / e_max;
+  values[5] = roundedness;
   rho = y_bar * cos(theta_1) - x_bar * sin( theta_1 );
+  values[6] = theta_1;
+  values[7] = rho;
+}
 
-
-
-
-
+bool writeDataBase ( const std::string& filename , const std::vector<double>& values){
+  FILE *output = fopen(filenamea,"w");
+  if (output == 0){
+    cout << "Write Database: cannot open file." << endl;
+    return false;
+  }
+  
+  fprintf(ouput, "%d %d %d %d %d %d %d %d", values)
 }
 
 
@@ -79,12 +91,14 @@ main(int argc, char **argv){
   const string output_img(argv[3]);
   size_t i , j;
   int pixel;
-  vector<int> labels;
+  std::vector<int> labels;
+  std::vector<std::vector<double>> values_;
+  
   
 
   Image an_image;
   if (!ReadImage(input_bin_img, &an_image)) {
-    cout <<"Can't open file " << input_file << endl;
+    cout <<"Can't open file " << input_fiAkkle << endl;
     return 0;
   }
   const int n_rows = an_image.num_rows();
@@ -100,6 +114,9 @@ main(int argc, char **argv){
       }
     }
   }
+
+  std::vector<std::vector<doubld>> values_ ( labels.size() ,
+                                            std::vector<double>(0,8)); 
 
   // row postions of center
 
